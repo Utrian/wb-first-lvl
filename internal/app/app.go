af0db190916f2fa92748178d9a0a1456d34574b0
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-05-14 12:12:24 +0300

    Changed internal/app.

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,11 +1,7 @@
 package app
 
-import (
-	"wb-first-lvl/internal/database/queries"
-	rec "wb-first-lvl/internal/services/nats-streaming/receive"
-)
+import rec "wb-first-lvl/internal/services/nats-streaming/receive"
 
 func Run() {
 	rec.Receiver()
-	queries.GetAllOrders()
 }
