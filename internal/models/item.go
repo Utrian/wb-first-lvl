59477241110ea255bec5cb84ad0e1b21d2b74250
Paul Nikolaevskiy <paulnikolaevsky@gmail.com>
2023-05-14 08:53:15 +0300

    Changed the models.

--- internal/models/item.go
+++ internal/models/item.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Item struct {
-	ChrtId      int    `json:"chrt_id" db:"chrt_id"`
-	TrackNumber string `json:"track_number" db:"track_number"`
-	Price       int    `json:"price" db:"price"`
-	Rid         string `json:"rid" db:"rid"`
-	Name        string `json:"name" db:"name"`
-	Sale        int    `json:"sale" db:"sale"`
-	Size        string `json:"size" db:"size"`
-	TotalPrice  int    `json:"total_price" db:"total_price"`
-	NmId        int    `json:"nm_id" db:"nm_id"`
-	Brand       string `json:"brand" db:"brand"`
-	Status      int    `json:"status" db:"status"`
+	ChrtId      int    `json:"chrt_id"`
+	TrackNumber string `json:"track_number"`
+	Price       int    `json:"price"`
+	Rid         string `json:"rid"`
+	Name        string `json:"name"`
+	Sale        int    `json:"sale"`
+	Size        string `json:"size"`
+	TotalPrice  int    `json:"total_price"`
+	NmId        int    `json:"nm_id"`
+	Brand       string `json:"brand"`
+	Status      int    `json:"status"`
 }
 
 func (i Item) Value() (driver.Value, error) {
--- internal/models/payment.go
+++ internal/models/payment.go
@@ -7,16 +7,16 @@ import (
 )
 
 type Payment struct {
-	Transaction  string `json:"transaction" db:"transaction"`
-	RequestId    string `json:"request_id" db:"request_id"`
-	Currency     string `json:"currency" db:"currency"`
-	Provider     string `json:"provider" db:"provider"`
-	Amount       int    `json:"amount" db:"amount"`
-	PaymentDt    int    `json:"payment_dt" db:"payment_dt"`
-	Bank         string `json:"bank" db:"bank"`
-	DeliveryCost int    `json:"delivery_cost" db:"delivery_cost"`
-	GoodsTotal   int    `json:"goods_total" db:"goods_total"`
-	CustomFee    int    `json:"custom_fee" db:"custom_fee"`
+	Transaction  string `json:"transaction"`
+	RequestId    string `json:"request_id"`
+	Currency     string `json:"currency"`
+	Provider     string `json:"provider"`
+	Amount       int    `json:"amount"`
+	PaymentDt    int    `json:"payment_dt"`
+	Bank         string `json:"bank"`
+	DeliveryCost int    `json:"delivery_cost"`
+	GoodsTotal   int    `json:"goods_total"`
+	CustomFee    int    `json:"custom_fee"`
 }
 
 func (p Payment) Value() (driver.Value, error) {
